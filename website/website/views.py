5b35e2fb36abafcbb26e8458a8ad69547184b9e9
郭坤 <kun.g@qq.com>
2019-11-19 16:30:03 +0800

    MD

--- website/website/views.py
+++ website/website/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # from . import wikipedia
-from .wikipedia import get_people
+from .wikipedia import get_people, render_md
 
 #data = get_people(wiki_ur)
 
@@ -12,6 +12,8 @@ def index(request):
 
 def wikipedia(request):
     if request.POST and request.POST['url']:
-        return HttpResponse(get_people(request.POST['url']))
+        data = get_people(request.POST['url'])
+        print(data)
+        return HttpResponse(render_md(data))
     else:
         return index(request)
